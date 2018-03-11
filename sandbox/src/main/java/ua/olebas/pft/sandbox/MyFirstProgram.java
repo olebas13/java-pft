6f82d0482e9078cbab1787b9c3e8d839c497c3ce
Olebas13 <o.nevoyt13@gmail.com>
2018-03-11 15:09:34 +0200

    удаление арифм операций

--- sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
@@ -3,20 +3,5 @@ package ua.olebas.pft.sandbox;
 public class MyFirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
-        System.out.println(1 / 2.0);
-
-        System.out.println("2" + "2");
-        System.out.println("2" + 2);
-
-        System.out.println(2 + 2 * 2);
-        System.out.println("2 + 2 = " + (2 + 2));
     }
 }
