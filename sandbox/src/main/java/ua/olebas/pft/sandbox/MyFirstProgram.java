876121f106da49737834ee0a662cd403d42de0de
Olebas13 <o.nevoyt13@gmail.com>
2018-03-11 17:58:30 +0200

    update program

--- sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,11 @@ package ua.olebas.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String somebody = "world";
+        System.out.println("Hello, " + somebody + "!");
+
+        double l = 8.0;
+        double s = l * l;
+        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
     }
 }
