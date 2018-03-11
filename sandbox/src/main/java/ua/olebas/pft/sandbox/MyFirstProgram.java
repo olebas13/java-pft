8f8deac2654eebc61e4683416cb38fb6958f01b4
Olebas13 <o.nevoyt13@gmail.com>
2018-03-11 15:06:41 +0200

    арифметика

--- sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,22 @@
 package ua.olebas.pft.sandbox;
 
 public class MyFirstProgram {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+        System.out.println(1 / 2.0);
+
+        System.out.println("2" + "2");
+        System.out.println("2" + 2);
+
+        System.out.println(2 + 2 * 2);
+        System.out.println("2 + 2 = " + (2 + 2));
+    }
 }
