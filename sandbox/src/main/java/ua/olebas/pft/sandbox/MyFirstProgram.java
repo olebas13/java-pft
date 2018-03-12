ff7e75c3031179de2b57d5a0d81f7a6adef4df18
Olebas13 <o.nevoyt13@gmail.com>
2018-03-12 14:36:47 +0200

    add several functions

--- sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ua/olebas/pft/sandbox/MyFirstProgram.java
@@ -2,11 +2,27 @@ package ua.olebas.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        String somebody = "world";
-        System.out.println("Hello, " + somebody + "!");
+        hello("Olebas");
+        hello("world");
+        hello("user");
 
-        double l = 8.0;
-        double s = l * l;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+        double len = 5;
+        System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
     }
 }
