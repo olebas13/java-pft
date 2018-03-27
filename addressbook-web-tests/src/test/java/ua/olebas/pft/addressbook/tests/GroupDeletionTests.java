06e2e053a64d1bea0ffa058f776a399cde16f9df
olebas1927 <olebas13@ukr.net>
2018-03-27 10:07:29 +0300

    реализована проверка и обеспечение выполнения предусловий

---
+++ addressbook-web-tests/src/test/java/ua/olebas/pft/addressbook/tests/GroupDeletionTests.java
@@ -0,0 +1,18 @@
+package ua.olebas.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ua.olebas.pft.addressbook.model.GroupData;
+
+public class GroupDeletionTests extends TestBase {
+
+    @Test
+    public void testGroupDeletion() {
+        app.getNavigationHelper().gotoGroupPage();
+        if (! app.getGroupHelper().isThereAGroup()) {
+            app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+        }
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().deleteSelectedGroups();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
