bbf2166828e7947854a8e9c2428c17bb84e08c79
olebas1927 <olebas13@ukr.net>
2018-04-03 13:15:26 +0300

    описание конфигурации тестового стенда

--- addressbook-web-tests/src/test/java/ua/olebas/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ua/olebas/pft/addressbook/appmanager/ApplicationManager.java
@@ -6,10 +6,15 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
     private SessionHelper sessionHelper;
     private NavigationHelper navigationHelper;
@@ -17,15 +22,14 @@ public class ApplicationManager {
     private ContactHelper contactHelper;
     private String browser;
 
-    public ApplicationManager(String browser) {
+    public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
-
-        // System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\webdrivers\\chromedriver\\chromedriver.exe");
-        // System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\webdrivers\\geckodriver\\geckodriver.exe");
-        // System.setProperty("webdriver.edge.driver", "C:\\Program Files\\webdrivers\\edgedriver\\MicrosoftWebDriver.exe");
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src\\test\\resources\\%s.properties", target))));
         if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver();
         }
@@ -38,12 +42,12 @@ public class ApplicationManager {
 
         wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         wd.manage().window().maximize();
-        wd.get("http://localhost/addressbook/");
+        wd.get(properties.getProperty("web.baseURL"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
     public void stop() {
